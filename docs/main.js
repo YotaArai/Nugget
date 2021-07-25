16763623a72ae57f3d4e858989c9724cc73477b7
YotaArai <yotaarai85@gmail.com>
2021-07-25 17:28:40 +0900

    fix: タイプライターアニメーションライブラリを削除、自前実装

--- docs/main.js
+++ docs/main.js
@@ -148,118 +148,6 @@ module.exports = function cssWithMappingToString(item) {
 
 /***/ }),
 
-/***/ "./node_modules/ityped/src/index.js":
-/*!******************************************!*\
-  !*** ./node_modules/ityped/src/index.js ***!
-  \******************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "init": () => (/* binding */ init)
-/* harmony export */ });
-/**
- * @name setProps
- * @description Set the ityped properties configuration
- * @param {Object} config The configuration properties
- * @return {Promise}
- */
-const setProps = ({
-  strings = ['Put your strings here...', 'and Enjoy!'],
-  typeSpeed = 100,
-  backSpeed = 50,
-  backDelay = 500,
-  startDelay = 500,
-  cursorChar = '|',
-  placeholder = false,
-  showCursor = true,
-  disableBackTyping = false,
-  onFinished = function () { },
-  loop = true
-}) => ({
-  strings,
-  typeSpeed,
-  backSpeed,
-  cursorChar,
-  backDelay,
-  placeholder,
-  startDelay,
-  showCursor,
-  loop,
-  disableBackTyping,
-  onFinished
-})
-
-const init = (element, properties) => {
-  let i = 0, l, STRINGS_TO_ITERATE;
-
-  const typewrite = (strings, props) => {
-    if (i === l)
-      if (props.loop) i = 0;
-    setTimeout(() => { typeString(strings[i], props); }, props.startDelay);
-  }
-
-  const typeString = (str, props) => {
-    let index = 0,
-      strLen = str.length;
-    let intervalID = setInterval(() => {
-      props.placeholder ? element.placeholder += str[index] : element.textContent += str[index];
-      if (++index === strLen) return onStringTyped(intervalID, props);
-    }, props.typeSpeed);
-  }
-
-  const onStringTyped = (id, props) => {
-    clearInterval(id);
-    if (props.disableBackTyping && i === l - 1) {
-      return props.onFinished()
-    }
-    if (!props.loop && i === l - 1) {
-      return props.onFinished();
-    }
-    setTimeout(() => eraseString(props), props.backDelay);
-  }
-
-  const eraseString = (props) => {
-    let str = props.placeholder ? element.placeholder : element.textContent,
-      strLen = str.length;
-    let intervalID = setInterval(() => {
-      props.placeholder
-        ? element.placeholder = element.placeholder.substr(0, --strLen)
-        : element.textContent = str.substr(0, --strLen);
-      if (strLen === 0) return onStringErased(intervalID, props);
-    }, props.backSpeed);
-  }
-
-  const onStringErased = (id, props) => {
-    clearInterval(id);
-    ++i;
-    typewrite(STRINGS_TO_ITERATE, props);
-  }
-
-  const setCursor = (element, props) => {
-    let cursorSpan = document.createElement('span');
-    cursorSpan.classList.add('ityped-cursor');
-    cursorSpan.textContent = '|';
-    cursorSpan.textContent = props.cursorChar;
-    element.insertAdjacentElement('afterend', cursorSpan);
-  }
-
-
-  const startTyping = (prop) => {
-    let props = setProps(prop || {})
-    let strings = props.strings
-    STRINGS_TO_ITERATE = strings
-    l = strings.length
-    if (typeof element === "string") element = document.querySelector(element)
-    if (props.showCursor) setCursor(element, props)
-    typewrite(strings, props)
-  }
-
-  return startTyping(properties)
-}
-
-/***/ }),
-
 /***/ "./src/index.scss":
 /*!************************!*\
   !*** ./src/index.scss ***!
@@ -50637,18 +50525,31 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (/* binding */ Text)
 /* harmony export */ });
-/* harmony import */ var ityped__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ityped */ "./node_modules/ityped/src/index.js");
-
 class Text {
     constructor() {
-        this.element = document.querySelector('#itype-text');
-        (0,ityped__WEBPACK_IMPORTED_MODULE_0__.init)(this.element, {
-            showCursor: false,
-            strings: ['これは おじさんの きんのたま!\nゆうこうに かつようして くれ!'],
-            disableBackTyping: false,
-            loop: false,
-            typeSpeed: 100,
-        });
+        this.text = "これは おじさんの きんのたま!\nゆうこうに かつようして くれ!";
+        this.element = document.getElementById("typing-animation");
+        this.intervalId = 0;
+        this.onLoad();
+        window.addEventListener("click", this.onClick.bind(this));
+    }
+    onLoad() {
+        let currentText = "";
+        const chars = Array.from(this.text);
+        let position = 0;
+        this.intervalId = window.setInterval(() => {
+            if (chars[position] == undefined) {
+                window.clearInterval(this.intervalId);
+                return;
+            }
+            currentText += chars[position];
+            this.element.innerText = currentText;
+            position++;
+        }, 70);
+    }
+    onClick() {
+        this.element.innerText = this.text;
+        window.clearInterval(this.intervalId);
     }
 }
 
--- src/text.ts
+++ src/text.ts
@@ -1,16 +1,33 @@
-import { init } from 'ityped'
-
 export default class Text{
   element: HTMLElement;
+  text: string;
+  intervalId: number;
 
   constructor() {
-    this.element = document.querySelector('#itype-text') as HTMLElement;
-    init(this.element, { 
-      showCursor: false,
-      strings: ['これは おじさんの きんのたま!\nゆうこうに かつようして くれ!'],
-      disableBackTyping: false,
-      loop: false,
-      typeSpeed: 100,
-    });
+    this.text = "これは おじさんの きんのたま!\nゆうこうに かつようして くれ!";
+    this.element = document.getElementById("typing-animation") as HTMLElement;
+    this.intervalId = 0;
+    this.onLoad();
+    window.addEventListener("click", this.onClick.bind(this));
+  }
+
+  onLoad() {
+    let currentText = "";
+    const chars = Array.from(this.text);
+    let position = 0;
+    this.intervalId = window.setInterval(() =>{
+      if (chars[position] == undefined) {
+        window.clearInterval(this.intervalId);
+        return;
+      }
+      currentText += chars[position]; 
+      this.element.innerText = currentText;
+      position++;
+    }, 70);
+  }
+      
+  onClick(){
+    this.element.innerText = this.text;
+    window.clearInterval(this.intervalId);
   }
 }
